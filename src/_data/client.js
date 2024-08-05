7d5b492925e329287daae3b472397ec93b6e00d8
Koza101 <153244678+Koza101@users.noreply.github.com>
2024-08-05 19:32:46 -0400

    Initial Changes

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,20 +1,20 @@
 module.exports = {
-    "name": "Code Stitch Web Designs",
-    "email": "help@codestitch.app",
-    "phoneForTel": "555-779-4407",
-    "phoneFormatted": "(555) 779-4407",
+    "name": "Champlain Valley Web Design",
+    "email": "help@cvwds.com",
+    "phoneForTel": "518-536-6894",
+    "phoneFormatted": "(518) 536-6894",
     "address": {
         "lineOne": "First Address Line",
         "lineTwo": "Second Address Line",
-        "city": "Denver",
-        "state": "CO",
-        "zip": "80206",
-        "mapLink": "https://goo.gl/maps/UAQn4vuGDiwv7DV39"
+        "city": "Plattsburgh",
+        "state": "NY",
+        "zip": "12901",
+        "mapLink": "https://www.google.com/maps/place/Plattsburgh,+NY/@44.6961772,-73.4773315,14z/data=!3m1!4b1!4m6!3m5!1s0x4cca38a8e8a57ed7:0xb5b0e8a1c5537415!8m2!3d44.6994873!4d-73.4529124!16zL20vMDFtYm5i?entry=ttu"
     },
     "socials": {
         "facebook": "https://www.facebook.com/",
         "instagram": "https://www.instagram.com/",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://www.example.com"
+    "domain": "https://www.cvwds.com"
 };
