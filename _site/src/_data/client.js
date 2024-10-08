973d921d713e2488536cbfb35098043f8f19a67e
Koza101 <masonrkozak@gmail.com>
2024-10-08 10:36:20 -0400

    Update sharp images eleventy config

---
+++ _site/src/_data/client.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "name": "Champlain Valley Web Design",
+    "email": "mkozak@cvwds.com",
+    "phoneForTel": "518-536-6894",
+    "phoneFormatted": "(518) 536-6894",
+    "address": {
+        "lineOne": "",
+        "lineTwo": "",
+        "city": "Plattsburgh",
+        "state": "NY",
+        "zip": "12901",
+        "mapLink": "https://www.google.com/maps/place/Plattsburgh,+NY/@44.6961772,-73.4773315,14z/data=!3m1!4b1!4m6!3m5!1s0x4cca38a8e8a57ed7:0xb5b0e8a1c5537415!8m2!3d44.6994873!4d-73.4529124!16zL20vMDFtYm5i?entry=ttu"
+    },
+    "socials": {
+        "facebook": "https://www.facebook.com/",
+        "instagram": "https://www.instagram.com/c_v_w_d",
+    },
+    //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
+    "domain": "https://cvwds.com"
+};
